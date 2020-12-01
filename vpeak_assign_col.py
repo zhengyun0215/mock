d5dc24a27b539bca4c6cda9d994ef2618a068771
zhengyun0215 <yunzheng@sjtu.edu.cn>
2020-12-01 10:34:50 +0800

    change the Process number

--- vpeak_assign_col.py
+++ vpeak_assign_col.py
@@ -9,18 +9,20 @@ redshift = np.loadtxt('/home/yunzheng/mock/orphan/redshift.txt')
 def vpeak(x):
     start = time.time()
     print('the redshift of snapshot %d is %s'%(x,redshift[x]))
-    sub_file = h5py.File('/home/cossim/CosmicGrowth/6610/subcat2/SubSnap_0%s.hdf5'%x,'r')['Subhalos'][...]
+    sub_file = h5py.File('/home/cossim/CosmicGrowth/6610/subcat2/SubSnap_%03d.hdf5'%x,'r')['Subhalos'][...]
     subid = sub_file['TrackId']
     vmax = sub_file['VmaxPhysical']
     birthsnap = sub_file['SnapshotIndexOfBirth']
     
-    
+    print("the length of subid is %d"%(len(subid)))
     a = np.stack((subid,vmax,birthsnap),axis= -1)
     
-    np.savetxt('/home/yunzheng/mock/color/data/vmax/snapshot_%s.dat'%x)
+    np.save('/home/yunzheng/mock/color/data/vmax/snapshot_%s.npy'%x,a)
+    end = time.time()
+    print("time spent : %s minutes"%((end - start)/60))
     
 def multicore():
-    pool = mp.Pool(processes = 64)
+    pool = mp.Pool(processes = 50)
     res = pool.map(vpeak,range(100))
 
     pool.close()
